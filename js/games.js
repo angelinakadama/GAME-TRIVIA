de056058533493c53eb7f6819428797ab0319424
angelinakadama <angelinakadama32@gmail.com>
2019-08-21 07:37:18 -0700

    stage one

---
+++ js/games.js
@@ -0,0 +1,132 @@
+{
+	"Games":
+     {
+     	"id":1,
+
+"Title - Bubble Poppers"
+"Platform"
+"Score - 8.0"
+"Genre - shooting"
+"Editors_choice-Yes"
+
+     }
+}
+
+{
+	"Games":
+	{
+		"id":1,
+
+	"Title - Fruit cut"
+"Platform"
+"Score - 5.0"
+"Genre - genre of the game"
+"Editors_choice-No"
+	
+	}
+
+}
+
+"Games":
+{
+	"id":1,
+	
+	"Title-car drive"
+	"Platform"
+	"Score- 4.0"
+	"Genre- racing"
+	"Editors_choice- Yes"
+     }
+}
+
+   "Games":
+    {
+    	"id":1,
+    	
+    	"Title-zuma"
+    	"Platform"
+    	"Score-9.0"
+    	"Genre-Thinking"
+    	"Editors_choice-Yes"
+    }
+  {
+	"Games":
+     {
+     	"id":1,
+
+"Title - Bubble Poppers"
+"Platform"
+"Score - 8.0"
+"Genre - shooting"
+"Editors_choice-Yes"
+}  
+
+    }
+{
+	"Games":
+     {
+     	"id":1,
+
+"Title - Bubble Poppers"
+"Platform"
+"Score - 8.0"
+"Genre - shooting"
+"Editors_choice-Yes"
+}
+
+   }
+{
+	"Games":
+     {
+     	"id":1,
+
+"Title - Bubble Poppers"
+"Platform"
+"Score - 8.0"
+"Genre - shooting"
+"Editors_choice-Yes"
+}
+   }
+
+{  
+	"Games":
+     {
+     	"id":1,
+
+"Title - Bubble Poppers"
+"Platform"
+"Score - 8.0"
+"Genre - shooting"
+"Editors_choice-Yes" 
+}
+     } 
+
+ {
+	"Games":
+     {
+     	"id":1,
+
+"Title - Bubble Poppers"
+"Platform"
+"Score - 8.0"
+"Genre - shooting"
+"Editors_choice-Yes"
+ }    
+    }
+
+ {
+	"Games":
+     {
+     	"id":1,
+
+"Title - Bubble Poppers"
+"Platform"
+"Score - 8.0"
+"Genre - shooting"
+"Editors_choice-Yes"
+ } 
+    } 
+
+ {
+ 	
+ }    
